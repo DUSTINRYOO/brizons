0101bac6fd622ffe7476c175e866a67c8cdaffeb
DUSTINRYOO <hyeony77@gmail.com>
2023-02-04 12:44:49 -0500

    Type ORM and Nest
    Data Mapper vs. Active Record
    Data mapper : including repository bigger app /maintainability
    
    -Injecting Repository

--- src/example/example.resolver.ts
+++ src/example/example.resolver.ts
@@ -1,13 +1,14 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { createExampleDto } from './dtos/create-example.dto';
 import { Example } from './entities/example.entity';
+import { ExampleService } from './example.service';
 
 @Resolver((of) => Example)
 export class ExampleResolver {
+  constructor(private readonly exampleService: ExampleService) {}
   @Query((returns) => [Example])
-  firstExample(@Args('firstOne') firstOne: Boolean): Example[] {
-    console.log(firstOne);
-    return [];
+  Examples(): Promise<Example[]> {
+    return this.exampleService.getAll();
   }
   @Mutation((returns) => Boolean)
   createExample(@Args() createExampleDto: createExampleDto): boolean {
