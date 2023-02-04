32786551fa6e1561b10cfe881eb59af4bf268ab9
DUSTINRYOO <hyeony77@gmail.com>
2023-02-03 23:05:15 -0500

    Apollo Server Setting
    -Use GraphQLModule.forRoot() in Nest js
    - Code First Approach (Not schema)
    - Example Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExampleModule } from './example/example.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+    }),
+    ExampleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
