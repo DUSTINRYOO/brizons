0f79ceeb85561d183f0f25c27dfd8194a0a5d19b
DUSTINRYOO <hyeony77@gmail.com>
2023-02-04 20:53:12 -0500

    Hashing Password

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,8 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -17,4 +19,17 @@ export class User extends CoreEntity {
   @Column()
   @Field((type) => String)
   email: string;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(
+        this.password,
+        +process.env.PW_SALTROUND,
+      );
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
+  }
 }
