9badda304a8f1ed2203d113f9cf87c7eefdb9518
DUSTINRYOO <hyeony77@gmail.com>
2023-03-05 23:49:08 -0500

    AWS S3 Setting
    S3 Bucket generation

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -5,12 +5,25 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
 
 @Controller('uploads')
 export class UploadsController {
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+    try {
+      const upload = await new AWS.S3()
+        .createBucket({ Bucket: 'dustinbrizonsbucketlfg' })
+        .promise();
+      console.log(upload);
+      console.log(file);
+    } catch {}
   }
 }
