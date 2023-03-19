4f4a1cd063156854eb6a34f0d3715626bdd1b293
DUSTINRYOO <hyeony77@gmail.com>
2023-03-18 21:55:00 -0400

    Create Text Entity

---
+++ src/brizs/entities/text.entity.ts
@@ -0,0 +1,50 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, JoinColumn, OneToOne, RelationId } from 'typeorm';
+import { Briz } from './briz.entity';
+
+@InputType('TextInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Text extends CoreEntity {
+  @Column()
+  @Field((type) => Number)
+  @IsNumber()
+  fontSize: number;
+
+  @Column()
+  @Field((type) => String)
+  @IsString()
+  bold: string;
+
+  @Column()
+  @Field((type) => Boolean)
+  @IsBoolean()
+  italic: boolean;
+
+  @Column()
+  @Field((type) => String)
+  @IsString()
+  textColor: string;
+
+  @Column()
+  @Field((type) => String)
+  @IsString()
+  boxColor: string;
+
+  @Column()
+  @Field((type) => String)
+  @IsString()
+  textColAlign: string;
+
+  @Column()
+  @Field((type) => String)
+  @IsString()
+  textRowAlign: string;
+
+  @OneToOne(() => Briz, (briz) => briz.grid, {
+    onDelete: 'CASCADE',
+  })
+  briz: Briz;
+}
